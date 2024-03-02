6b5fd85c79fc68bbbe491a3520a838d94e5996b9
Chhaylay <160891308+Chhaylayh@users.noreply.github.com>
2024-03-02 12:08:13 -0600

    Update Memory.java

--- CS 4348 - Operating Systems Concepts/Project 1/Memory.java
+++ CS 4348 - Operating Systems Concepts/Project 1/Memory.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Memory {
     
+	// Array of integers for memory
 	static int[] memory;
 
     public static void main(String[] args) {
 
+		// If there are no input, exit
         if (args.length < 1) {
             System.err.println("Not Enough Arguments");
             System.exit(1);
@@ -23,6 +25,7 @@ public class Memory {
             System.exit(1);
         }
 
+		// Process input from the parents process
         Scanner input = new Scanner(System.in);
        
         while (input.hasNextLine()) {
@@ -51,6 +54,7 @@ public class Memory {
         input.close();
     }
 
+	// Allocates memory and loads the program from file
     private static void initializeMemory(String inputFilePath) throws FileNotFoundException {
         
 		memory = new int[2000];
@@ -61,36 +65,44 @@ public class Memory {
             
 			String line = scan.nextLine().trim();
             
+			// If empty, skip
             if (line.length() < 1)
                 continue;
             
+			// For line starting with ".", move to the position in the memory
             if (line.charAt(0) == '.') {
                 memIndex = Integer.parseInt(line.substring(1).split("\\s+")[0]);
                 continue;
             }
             
+			// If first part is not a number, skip
             if (line.charAt(0) < '0' || line.charAt(0) > '9') 
                 continue;
             
+			// Split by whitespace to first number
             String[] split = line.split("\\s+");
            
-            if (split.length < 1)
+            // If empty, skip
+			if (split.length < 1)
                 continue;
-            else 
+            else	// Read the first integer into memory
                 memory[memIndex++] = Integer.parseInt(split[0]);
         }
         scan.close();
     }
 
+	// Log Class
     private static void log(String str) {
         
     }
 
+	// Reads address
     private static int read(int address) {
         log("Reading " + address + " = " + memory[address]);
         return memory[address];
     }
 
+	// Writes data
     private static void write(int address, int data) {
         log("Writing " + data + " to " + address);
         memory[address] = data;
