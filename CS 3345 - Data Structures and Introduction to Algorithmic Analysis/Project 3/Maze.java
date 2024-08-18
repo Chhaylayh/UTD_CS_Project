71f4d9a52dbcb16c2d5c1a1e017fc6337d23f68e
Chhaylay <160891308+Chhaylayh@users.noreply.github.com>
2024-08-18 10:17:04 -0500

    Update Maze.java

--- CS 3345 - Data Structures and Introduction to Algorithmic Analysis/Project 3/Maze.java
+++ CS 3345 - Data Structures and Introduction to Algorithmic Analysis/Project 3/Maze.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Maze {
+    
     private int[][] maze;
     private int n;
     private int m;
@@ -14,7 +15,6 @@ public class Maze {
     }
     
     public void generateMaze() {
-        
         // Initialize maze with all walls present
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -31,21 +31,20 @@ public class Maze {
     }
     
     private void generateMazeRecursive(int x, int y)  {
+        
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, 1, 0, -1};
         List<Integer> directions = Arrays.asList(0, 1, 2, 3);
         Collections.shuffle(directions, rand);
         
-        for (int d : directions) 
-        {
+        for (int d : directions) {
             int nx = x + dx[d];
             int ny = y + dy[d];
             
             if (nx < 0 || ny < 0 || nx >= n || ny >= m) 
                 continue;
             
-            if (maze[nx][ny] == 15) 
-            {
+            if (maze[nx][ny] == 15) {
                 // Remove wall between current cell and neighbor
                 int wallToRemove = 1 << d;
                 maze[x][y] &= ~wallToRemove;
@@ -55,20 +54,16 @@ public class Maze {
         }
     }
     
-    public void printMaze() 
-    {
-        for (int i = 0; i < n; i++) 
-        {
-            for (int j = 0; j < m; j++) 
-            {
+    public void printMaze() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 System.out.print(String.format("%x", maze[i][j]));
             }
             System.out.println();
         }
     }
     
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args)  {
         Maze mazeGenerator = new Maze(10, 10);
         mazeGenerator.generateMaze();
         mazeGenerator.printMaze();
