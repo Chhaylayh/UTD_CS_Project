f2d197cbddaa01154146bdb22b8a9f2f3e9cebb3
Chhaylay <160891308+Chhaylayh@users.noreply.github.com>
2024-08-18 10:19:11 -0500

    Update MazeSolver.java

--- CS 3345 - Data Structures and Introduction to Algorithmic Analysis/Project 3/MazeSolver.java
+++ CS 3345 - Data Structures and Introduction to Algorithmic Analysis/Project 3/MazeSolver.java
@@ -1,26 +1,24 @@
-public class MazeSolver 
-{
+public class MazeSolver {
+    
     private int[][] maze;
     private int n;
     private int m;
     private String path;
     
-    public MazeSolver(int[][] maze) 
-    {
+    public MazeSolver(int[][] maze) {
         this.maze = maze;
         this.n = maze.length;
         this.m = maze[0].length;
         this.path = "";
     }
     
-    public boolean solveMaze() 
-    {
+    public boolean solveMaze() {
         boolean[][] visited = new boolean[n][m];
         return solveMazeRecursive(0, 0, visited);
     }
     
-    private boolean solveMazeRecursive(int x, int y, boolean[][] visited)
-    {
+    private boolean solveMazeRecursive(int x, int y, boolean[][] visited){
+        
         if (x == n-1 && y == m-1)
             return true;
        
@@ -29,8 +27,7 @@ public class MazeSolver
         int[] dy = {0, 1, 0, -1};
         char[] directions = {'S', 'E', 'N', 'W'};
         
-        for (int i = 0; i < 4; i++) 
-        {
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             
@@ -39,10 +36,8 @@ public class MazeSolver
             
             int wall = 1 << i;
             
-            if ((maze[x][y] & wall) == 0 && !visited[nx][ny]) 
-            {
-                if (solveMazeRecursive(nx, ny, visited)) 
-                {
+            if ((maze[x][y] & wall) == 0 && !visited[nx][ny]) {
+                if (solveMazeRecursive(nx, ny, visited)) {
                     path += directions[i];
                     return true;
                 }
@@ -51,23 +46,20 @@ public class MazeSolver
         return false;
     }
     
-    public String getPath() 
-    {
+    public String getPath() {
         return path;
     }
     
-    public static void main(String[] args) 
-    {
-        int[][] maze = 
-        {
-            {10, 5, 10, 9, 8, 8, 9, 12},
-            {6, 6, 12, 3, 9, 9, 12, 2},
-            {2, 3, 6, 12, 3, 10, 3, 10},
-            {4, 3, 3, 10, 5, 3, 10, 3},
-            {6, 10, 9, 8, 10, 5, 5, 5},
-            {5, 5, 5, 5, 5, 5, 5, 5},
-            {5, 5, 5, 5, 5, 5, 5, 5},
-            {5, 5, 5, 5, 5, 5, 5, 5}
+    public static void main(String[] args)  {
+        
+        int[][] maze =  { {10, 5, 10, 9, 8, 8, 9, 12},
+                          {6, 6, 12, 3, 9, 9, 12, 2},
+                          {2, 3, 6, 12, 3, 10, 3, 10},
+                          {4, 3, 3, 10, 5, 3, 10, 3},
+                          {6, 10, 9, 8, 10, 5, 5, 5},
+                          {5, 5, 5, 5, 5, 5, 5, 5},
+                          {5, 5, 5, 5, 5, 5, 5, 5},
+                          {5, 5, 5, 5, 5, 5, 5, 5}
         };
         
         MazeSolver mazeSolver = new MazeSolver(maze);
